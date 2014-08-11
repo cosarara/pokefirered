d821296ef49f2ef0680b7ad832241bdee6368788
Jaume Delclòs Coll <cosarara97@gmail.com>
2014-08-11 16:28:58 +0200

    Utils script to be sourced from the shell

---
+++ utils.sh
@@ -0,0 +1,18 @@
+
+function dis_t {
+	arm-none-eabi-objdump -b binary -m arm -M force-thumb \
+		--start-address=$2 --stop-address=$3 \
+		-D $1 | sed 's/;/\/\//' | cut -f3,4,5,6,7,8
+}
+
+function dis_t_ {
+	arm-none-eabi-objdump -b binary -m arm -M force-thumb \
+		--start-address=$2 --stop-address=$3 \
+		-D $1 | sed 's/;/\/\//'
+}
+
+function sub {
+	python -c "print(hex($1 - $2))"
+}
+
+
